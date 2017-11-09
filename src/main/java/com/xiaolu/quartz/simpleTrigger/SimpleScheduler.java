ac82d851ab5340f03f92c76709d36e30006249ec
chiyu6028 <chiyu6028@126.com>
2017-11-09 10:20:19 +0800

    CronTrigger的运用。

--- src/main/java/com/xiaolu/quartz/simpleTrigger/SimpleScheduler.java
+++ src/main/java/com/xiaolu/quartz/simpleTrigger/SimpleScheduler.java
@@ -12,19 +12,23 @@ public class SimpleScheduler {
 
     public static void main(String[] args) {
         try {
+            //第一步，创建dobDetail
             JobDetail jobDetail = JobBuilder.newJob(SimpleJob.class).withIdentity("job","jgroup").build();
             JobDataMap jobDataMap = jobDetail.getJobDataMap();
             jobDataMap.put("hello","world");
 
+            //第二步，创建触发器
             SimpleScheduleBuilder simpleScheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
                     .withIntervalInSeconds(5).withRepeatCount(3);
 
             SimpleTrigger simpleTrigger = TriggerBuilder.newTrigger().withIdentity("trigger","tgroup")
                     .startNow().withSchedule(simpleScheduleBuilder).build();
 
-
+            //第三步，创建schedule
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.scheduleJob(jobDetail,simpleTrigger);
+
+            //第四步，启动
             scheduler.start();
 
         } catch (Exception e) {
