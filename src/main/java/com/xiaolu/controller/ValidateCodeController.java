100675b1e0f9d291281e3c7312ef82063f6891d0
chiyu6028 <chiyu6028@126.com>
2017-11-15 18:13:55 +0800

    验证码随机数字去掉 o 0 O

--- src/main/java/com/xiaolu/controller/ValidateCodeController.java
+++ src/main/java/com/xiaolu/controller/ValidateCodeController.java
@@ -26,10 +26,10 @@ public class ValidateCodeController {
     private int fontHeight = 18;
     private int codeY = 16;
     char[] codeSequence = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+            'K', 'L', 'M', 'N',  'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
             'X', 'Y', 'Z', 'a', 'b', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-            'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+            'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     @RequestMapping("/validateCode")
     public void getCode(HttpServletRequest req, HttpServletResponse resp)
